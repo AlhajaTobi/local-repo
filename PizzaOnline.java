4fb82706273ae4f8c96d30c612444ba4acd2e41f
AlhajaTobi <adeboyetobi@yahoo.com>
2024-09-01 14:15:16 -0700

    Add files via upload

---
+++ PizzaOnline.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class PizzaOnline {
+      public static void main(String...args) {
+      Scanner scanner = new Scanner(System.in);
+
+     System.out.print("Enter number of people ordering for: ");
+     int numberOfPeople = scanner.nextInt();
+     scanner.nextLine();
+ 
+    System.out.print(" Enter the pizza type:  \n Sapa, \n Small Money,\n Big Boys, \n Odogwu");
+    String pizzaType = scanner.nextLine();
+
+   int slicesPerBox = 0;
+   int pricePerBox  = 0;
+
+   switch (pizzaType)  {
+       case "Sapa":
+             slicesPerBox = 4;
+             pricePerBox  = 2000;
+             break;
+      case  "Small Money":
+          slicesPerBox = 6;
+          pricePerBox  = 2400;
+          break;
+      case "Big Boys":
+          slicesPerBox = 8;
+          pricePerBox  = 3000;
+          break;
+      case "Odogwu":
+            slicesPerBox = 12;
+            pricePerBox  = 4200;
+            break;
+        default:
+              System.out.println("Invalid pizza type order");
+              return;
+    }
+     
+     
+       int pizzaBoxNeeded = (int) Math.ceil((double) numberOfPeople / slicesPerBox);
+       int totalSlices = pizzaBoxNeeded * slicesPerBox;
+       int leftoverSlices = totalSlices - numberOfPeople;
+       int totalCost = pizzaBoxNeeded * pricePerBox;
+       System.out.println("Number of boxes of pizza to buy: " + pizzaBoxNeeded);
+       System.out.println("Number of leftover slices:  " + leftoverSlices);
+       System.out.println("Total cost: NGN " + totalCost);
+
+     scanner.close();
+
+
+   }
+}       
+   
+
+   
